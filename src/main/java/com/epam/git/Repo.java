a858ce84c38902cfb0ba882b200c7efa25ff6e5f
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2024-01-16 05:34:36 +0400

    Update

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/RamazArtmeladze/git.git";
+    public static String REPO_LINK = "https://github.com/RamazArtmeladze/gitTask";
 }
